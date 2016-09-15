7361e58bb90d7264d7a105eea89d60784195dc55
carlos gabriel <carlosgabriel71@yahoo.com>
2016-09-15 12:23:44 -0700

    Added document_vertical_scrollbar_visibility.js snippet to js/detection/ directory.

---
+++ js/detection/document_vertical_scrollbar_visibility.js
@@ -0,0 +1,16 @@
+/**
+ * @description [Checks whether document's vertical scrollbar is visible.]
+ * @return {Boolean}
+ * @info [http://stackoverflow.com/questions/2146874/detect-if-a-page-has-a-vertical-scrollbar]
+ */
+function is_vertical_scrollbar_visible() {
+    return (document.body.clientHeight > window.innerHeight);
+}
+
+// usage
+if (is_vertical_scrollbar_visible()) {
+    console.log("visible :)");
+} else {
+    // hidden :(
+    console.log("hidden :(");
+}
