bdf1b44370da3497aee34646a2151edcc7e9793a
carlos gabriel <carlosgabriel71@yahoo.com>
2016-11-13 19:39:03 -0800

    refactor: Add comment checking
    Algorithm is now aware of comments in CSS.
    
    {master}[snippets/concepts/css_dupe_finder]

--- concepts/css_dupe_finder/app_rewritten.js
+++ concepts/css_dupe_finder/app_rewritten.js
@@ -21,6 +21,30 @@ document.onreadystatechange = function() {
         // loop over string
         for (var i = 0, l = string.length; i < l; i++) {
 
+            // console.log(i);
+
+            // cache the current character in loop
+            var char = string.charAt(i),
+                char_code = char.charCodeAt(0);
+
+            // look out for these characters: @, {, }, /*, */
+
+            // check for comments, opening to potential comment
+            // if a comment is found the entire comment is skipped
+            // by forwarding the loop index to the position of the
+            // closing comment characters + 2
+            if (char_code === 47) { // character: /
+                // check that the next character is an asterisk
+                if (string.charCodeAt(i + 1) === 42) { // character: *
+                    // **we have the opening of a comment**
+                    // look for the closing comment...
+                    var closing_comment_index = string.indexOf("*/", (i + 1));
+                    console.log("THIS IS THE COMMENT:", string.substring(i, (closing_comment_index + 2)));
+                    // skip loop all the way to the position of the ending closing comment characters
+                    i = closing_comment_index + 2;
+                }
+            }
+
         }
 
     }
