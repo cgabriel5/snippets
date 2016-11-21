922990b489f31edac3d543f44008524961e98e07
carlos gabriel <carlosgabriel71@yahoo.com>
2016-11-21 00:56:53 -0800

    chore: Update CSS dupe finder
    Separate log and CSS dupe block format logic. This way they are in
    their own functions and only do their specific function. Helps
    keep code clean and modular.
    
    {master}[snippets/concepts/css_dupe_finder]

--- concepts/css_dupe_finder/app_rewritten.js
+++ concepts/css_dupe_finder/app_rewritten.js
@@ -15,35 +15,68 @@ document.onreadystatechange = function() {
      * @return {Null}        [Function does not return anything. Only logs duplicates info
      *                        onto the console.]
      */
-    function log_dupes(blocks) {
+    function format(blocks) {
+
+        // formated blocked container
+        var formated = [];
 
         // loop over blocks
         for (var i = 0, l = blocks.length; i < l; i++) {
 
-            var block = blocks[i];
-            var selector = block[0];
-            // var css_text = block[1];
-            var dupes = block[2][0];
+            var block = blocks[i],
+                selector = block[0],
+                selectors = selector.split(" / "),
+                // var css_text = block[1];
+                dupes = block[2][0],
+                // calculate the CSS block indentation by simply repeating
+                // the \t (tab) character
+                indentation = Array((selectors.length) + 1).join("\t");
 
-            console.log(selector);
+            // build the selector
+            var build = [],
+                ending_braces = [],
+                indent;
+            for (var j = 0, ll = selectors.length; j < ll; j++) {
+                // calculate the selector indentation
+                indent = (Array((j) + 1).join("\t"));
+                // add indented selector + indented closing brace to
+                // their respective arrays
+                build.push(indent + selectors[j] + " {" + "\n");
+                ending_braces.push(indent + "}" + "\n");
+            }
 
             // loop over the duplicate properties
             for (var prop in dupes) {
-
                 if (dupes.hasOwnProperty(prop)) {
-
                     // get the dupes
                     var dupe_array = dupes[prop];
-
                     // loop over every dupe prop within each property and log
                     for (var j = 0, ll = dupe_array.length; j < ll; j++) {
                         var dupe_declaration = dupe_array[j];
-                        console.log(dupe_declaration[0] + ":", dupe_declaration[1] + ";");
+                        build.push(indentation + dupe_declaration[0] + ":", dupe_declaration[1] + ";\n");
                     }
-
                 }
-
             }
+
+            // join selector, CSS declarations, and closing braces to make
+            // the final string, then add to formated array
+            formated.push(build.concat(ending_braces.reverse()).join(""));
+
+        }
+
+        // return the formated dupes
+        return formated;
+
+    }
+
+    function log(dupes) {
+
+        // log the dupes length
+        console.log(dupes.length);
+
+        for (var i = 0, l = dupes.length; i < l; i++) {
+            // log the dupe
+            console.log(dupes[i]);
             // log a space between dupes
             console.log("");
         }
@@ -71,10 +104,8 @@ document.onreadystatechange = function() {
             var actions = {
                 "done": function() {
 
-                    var dupes = message.dupes;
-                    // log dupes to the console
-                    console.log(dupes.length);
-                    log_dupes(dupes);
+                    // format the dupes + log dupes to the console
+                    log(format(message.dupes));
 
                     // terminate worker
                     // worker.terminate(); // close worker from main file
