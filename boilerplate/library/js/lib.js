19a9860f2ecabe019f588c3b5fa7b63ee6bac0d8
carlos gabriel <carlosgabriel71@yahoo.com>
2017-01-21 15:48:54 -0800

    refactor: Update boilerplate lib.js
    An the id() helper function. This function generates a random ID.
    
    {master}[snippets/code_snippets/boilerplate/library/js/lib.js]

--- boilerplate/library/js/lib.js
+++ boilerplate/library/js/lib.js
@@ -7,6 +7,17 @@
         // =============================== Helper Functions
 
         /**
+         * @description [Generates a simple ID containing letters and numbers.]
+         * @param  {Number} length [The length the ID should be. Max length is 22 characters]
+         * @return {String}        [The newly generated ID.]
+         * @source {http://stackoverflow.com/a/38622545}
+         */
+        function id(length) {
+
+            return Math.random().toString(36).substr(2, length);
+
+        }
+        /**
          * @description [Returns index of given value in provided array.]
          * @param  {Array}    array [The array to check against.]
          * @param  {Integer}  value [The value to check.]
