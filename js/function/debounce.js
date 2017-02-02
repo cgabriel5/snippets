3cbbdee4fc9d8ccbe6dfd16c1bc19adfc6d4da0d
carlos gabriel <carlosgabriel71@yahoo.com>
2017-02-01 17:28:13 -0800

    chore: Upload js/function directory
    The directory will contain function code snippets. At the moment
    the directory contains a debounce and throttle snippet.
    
    {master}[snippets/code_snippets/js/function/]

---
+++ js/function/debounce.js
@@ -0,0 +1,36 @@
+/**
+ * @description [Debounces provided function.]
+ * @param  {Function} func            [The function to debounce.]
+ * @param  {Number} time              [The time to debounce by.]
+ * @param  {Object} scope             [The scope in which to run function with.]
+ * @param  {Boolean} run_immediately  [Flag indicating whether the function
+ *                                     should run immediately.]
+ * @return {Function}                 [The new debounced function.]
+ * @source debouncing function from John Hann
+ * @source {http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/}
+ * @source {https://www.paulirish.com/2009/throttled-smartresize-jquery-event-handler/}
+ * @resource [Another debounce function] {https://davidwalsh.name/javascript-debounce-function}
+ */
+function debounce(func, time, scope, run_immediately) {
+    var timeout;
+    return function() {
+        var context = (scope || this),
+            args = arguments;
+
+        function delayed() {
+            if (!run_immediately) {
+                func.apply(context, args);
+            }
+            timeout = null;
+        };
+        if (timeout) {
+            clearTimeout(timeout);
+        } else if (run_immediately) {
+            func.apply(context, args);
+        }
+        timeout = setTimeout(delayed, time || 100);
+    };
+}
+
+// usage
+var fn = debounce(function() { /* logic */ });
---
+++ js/function/throttle.js
@@ -0,0 +1,32 @@
+/**
+ * @description [Throttles provided function.]
+ * @param  {Function} func            [The function to throttle.]
+ * @param  {Number} time              [The time to throttle by.]
+ * @param  {Object} scope             [The scope in which to run function with.]
+ * @return {Function}                 [The new throttled function.]
+ * @source {https://remysharp.com/2010/07/21/throttling-function-calls}
+ */
+function throttle(func, time, scope) {
+    time || (time = 250);
+    var last,
+        deferTimer;
+    return function() {
+        var context = (scope || this);
+        var now = +new Date,
+            args = arguments;
+        if (last && now < last + time) {
+            // hold on to it
+            clearTimeout(deferTimer);
+            deferTimer = setTimeout(function() {
+                last = now;
+                func.apply(context, args);
+            }, time);
+        } else {
+            last = now;
+            func.apply(context, args);
+        }
+    };
+}
+
+// usage
+var fn = throttle(function() { /* logic */ });
