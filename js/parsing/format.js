06274c3e8404f4cd1615b5c7f909b832f1ff6722
cgabriel5 <carlosgabriel71@yahoo.com>
2017-05-10 09:55:58 -0700

    chore: Add format.js to code_snippets/js/parsing/ directory
    Format.js contains a function that formats the provided template
    with the provided data object.
    
    {master}[snippets/code_snippets/boilerplate/code_snippets/js/parsing/]

---
+++ js/parsing/format.js
@@ -0,0 +1,21 @@
+/**
+ * @description [Formats template with provided data object.]
+ * @param  {String} template [The template to use.]
+ * @param  {Object} data     [The object containing the data to replace placeholders with.]
+ * @return {Undefined}  [Nothing is returned.]
+ */
+function format(template, data) {
+    return template.replace(/\{\{(.*?)\}\}/g, function(match) {
+        match = match.replace(/^\{\{|\}\}$/g, "");
+        return data[match] ? data[match] : match;
+    });
+}
+
+// examples
+
+// the data object
+var data = { "first-name": "John", "last-name": "Doe" };
+// the string to format
+var string = "Hello, my name is {{first-name}} {{last-name}}";
+// format the string
+var formatted = format(string, data);
