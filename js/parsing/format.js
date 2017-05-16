133049155b6fab539c48968ea3ebec14204bdc40
cgabriel5 <carlosgabriel71@yahoo.com>
2017-05-15 23:45:00 -0700

    refactor: Update format.js
    Add a needed hash to placeholder.
    
    {master}[snippets/code_snippets/js/parsing/format.js]

--- js/parsing/format.js
+++ js/parsing/format.js
@@ -5,16 +5,17 @@
  * @return {Undefined}  [Nothing is returned.]
  */
 function format(template, data) {
-    return template.replace(/\{\{(.*?)\}\}/g, function(match) {
-        match = match.replace(/^\{\{|\}\}$/g, "");
+    return template.replace(/\{\{\#(.*?)\}\}/g, function(match) {
+        match = match.replace(/^\{\{\#|\}\}$/g, "");
         return data[match] ? data[match] : match;
     });
 }
-
 // examples
-
 // the data object
-var data = { "first-name": "John", "last-name": "Doe" };
+var data = {
+    "first-name": "John",
+    "last-name": "Doe"
+};
 // the string to format
 var string = "Hello, my name is {{first-name}} {{last-name}}";
 // format the string
