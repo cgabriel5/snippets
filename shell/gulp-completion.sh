d3ef28d2471340fabc2406859412ae03dcac0630
cgabriel5 <carlosgabriel71@yahoo.com>
2017-08-29 10:55:46 -0700

    refactor: Update gulp-completion.sh
    - Comment tweaks.
    - Minor code tweaks.
    
    {master}[snippets/application/shell]

--- shell/gulp-completion.sh
+++ shell/gulp-completion.sh
@@ -1,24 +1,29 @@
+#!/bin/bash
+
 # original code: [https://gist.github.com/leventebalogh/ba862a284d21c39a80ed03af7be128b5]
-# Inspired by the example in the above link. This version is simplified for speed.
+# grunt auto-completion: [https://github.com/gruntjs/grunt-cli/blob/master/completion/bash]
+# Inspired by the examples above. This version is simplified for speed.
 #
 # How It Works:
 # Looks for the first gulpfile.js file. If a gulpfile.js is found the task names are extracted
-# and used for auto-completion. This version allows for double and single quotes in
-# gulp.task("|'...
+# and used for auto-completion. This version allows for double and single quotes in gulp.task.
+# For example, gulp.task("default", function() {}); and gulp.task('default', function() {});
+# both work fine.
 #
 # How to use:
 # 1: Add the following line to your .bashrc/.bash_profile file: source ~/.gulp-completion.sh
 # 2: Create ~/.gulp-completion.sh and place the contents of this file in it.
 # 3: Restart terminal.
+# 4. Use auto-completion like you would anything else.
 #
 function _gulp_completions() {
 
     # Define vars.
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    current_path=$(pwd)
-    gulp_file=""
+    local cur="${COMP_WORDS[COMP_CWORD]}" # The currently typed in word to auto-complete.
+    current_path=$(pwd) # cache the current working directory
+    gulp_file="" # initiate gulp_file variable
 
-    # Find the gulpfile.js path.
+    # Find the first gulpfile.js file path to use.
     while [ "${current_path}" != "/" ]; do
         # -maxdepth: only search current directory level
         # -iname:    case insensitive name
@@ -37,9 +42,9 @@ function _gulp_completions() {
     if [ -z "${gulp_file}" ]; then return; fi;
 
     # Get all gulp tasks in the files (allows for either single or double quotes)
-    gulp_tasks=`grep -rhio "gulp\.task([\"|'][^\"']*" ${gulp_file} | sed "s/gulp.task([\"|']//g"`
+    gulp_tasks=$(grep -rhio "gulp\.task([\"|'][^\"']*" ${gulp_file} | sed "s/gulp.task([\"|']//g")
 
-    # Without this autocompletion would not work for colons.
+    # Without this auto-completion would not work for colons.
     COMP_WORDBREAKS="${COMP_WORDBREAKS//:}"
 
     # Tell complete what stuff to show.
