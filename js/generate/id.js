7df220ea93f9aa64dc90b332b316be9e12f52f32
cgabriel5 <carlosgabriel71@yahoo.com>
2018-08-11 10:29:49 -0700

    refactor: Cleanup code/documentation and improve the function

--- js/generate/id.js
+++ js/generate/id.js
@@ -1,12 +1,41 @@
 /**
- * @description [Generates a simple ID containing letters and numbers.]
- * @param  {Number} length [The length the ID should be. Max length is 22 characters]
- * @return {String}        [The newly generated ID.]
- * @source {http://stackoverflow.com/a/38622545}
+ * Generates a simple ID containing letters and numbers.
+ *
+ * @param  {number} length - The length the ID should be.
+ * @return {string} - The newly generated ID.
+ *
+ * @resource [http://stackoverflow.com/a/38622545]
  */
-function id(length) {
-    return Math.random().toString(36).substr(2, length);
-}
+var id = function(length) {
+	// Default to 10.
+	length = length || 10;
 
-// usage
-var uid = id(20); // i.e. --> "zprotguorwevih7vgvydgq"
+	// Calculate the numbers of loops needed.
+	var iterations = Math.floor(length / 10);
+	if (length % 10) {
+		// Increment the loop by 1.
+		iterations++;
+	}
+
+	// Store the generated strings here.
+	var strings = [];
+
+	// Generate the strings.
+	for (let i = 0, l = iterations; i < l; i++) {
+		strings.push(
+			Math.random()
+				.toString(36)
+				.substr(2, 10)
+		);
+	}
+
+	// Combine the strings.
+	var string = strings.join("");
+
+	// Finally, cut the string to desired length.
+	return string.substring(0, length);
+};
+
+// Usage.
+
+var uid = id(20); // "yh6zsfikuj6fu7yje57x"
