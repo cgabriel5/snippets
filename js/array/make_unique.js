27c2a1f6e599a3f3500861e558b71e5ee43e53c1
cgabriel5 <carlosgabriel71@yahoo.com>
2018-08-11 10:25:43 -0700

    refactor: Cleanup make_unique.js code/documentation

--- js/array/make_unique.js
+++ js/array/make_unique.js
@@ -1,18 +1,21 @@
 /**
- * @description [Make the provided array unique.]
- * @param  {Array} array       [The array to clean.]
- * @param  {Boolean} flag_sort [Flag indicating whether the array needs to be sorted.]
- * @return {Array}   [The worked on array.]
- * @source [http://stackoverflow.com/questions/1960473/unique-values-in-an-array/39272981#39272981]
- * @source [http://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly/21595293#21595293]
+ * Make the provided array unique.
+ *
+ * @param  {array} array - The array to clean.
+ * @param  {boolean} flag_sort - Flag indicating whether the array needs to be sorted.
+ * @return {array} - The worked on array.
+ *
+ * @resource [http://stackoverflow.com/questions/1960473/unique-values-in-an-array/39272981#39272981]
+ * @ersource [http://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly/21595293#21595293]
  */
-function make_unique(array, flag_sort) {
-    // make array unique
+var make_unique = function(array, flag_sort) {
+    // Make array unique.
     array = array.filter(function(x, i, a_) {
         return a_.indexOf(x) === i;
     });
-    // sort the array if flag set
-    // **Note: does not sort numbers
+
+    // Sort the array if flag set.
+    // **Note: does not sort numbers.
     if (flag_sort) {
         if (flag_sort === "alpha") {
             array = array.sort(function(a, b) {
@@ -24,26 +27,25 @@ function make_unique(array, flag_sort) {
             });
         }
     }
-    // return the array
+
+    // Return the array.
     return array;
-}
+};
 
-// ==================================================
-
-// examples
+// Usage.
 
 var list = ["car", "array", "zombie", "apple", "array", "bat", "array"];
-list = make_unique(list, "alpha"); // make unique and sort (alpha)
-// output: ["apple", "array", "bat", "car", "zombie"]
+list = make_unique(list, "alpha"); // Make unique and sort (alpha).
+// Output: ["apple", "array", "bat", "car", "zombie"]
 
 var list = ["car", "array", "zombie", "apple", "array", "bat", "array"];
-list = make_unique(list); // make unique
-// output: ["car", "array", "zombie", "apple", "bat"]
+list = make_unique(list); // Make unique.
+// Output: ["car", "array", "zombie", "apple", "bat"]
 
 var list = [1, 2, 4, 1, 6, 7, 2, 4, 3, 6];
 list = make_unique(list);
-// output: [1, 2, 4, 6, 7, 3]
+// Output: [1, 2, 4, 6, 7, 3]
 
 var list = [1, 2, 4, 1, 6, 7, 2, 4, 3, 6];
 list = make_unique(list, "number");
-// output: [1, 2, 3, 4, 6, 7]
+// Output: [1, 2, 3, 4, 6, 7]
