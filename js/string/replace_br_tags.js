c0e77f4d01a8647d64c31e645a938f7782569b33
cgabriel5 <carlosgabriel71@yahoo.com>
2018-09-29 13:42:18 -0700

    chore: Update replace_br_tags function

--- js/string/replace_br_tags.js
+++ js/string/replace_br_tags.js
@@ -1,6 +1,20 @@
 /**
- * Replace all br tags...
+ * Replace all br tags.
+ *
+ * @param  {string} string - The string to work.
+ * @param  {string} needle - The replacement string.
+ * @return {string} - The modified string.
  *
  * @resource [https://stackoverflow.com/a/5959455]
  */
+var replace_br_tags = function(string, replacement) {
+	return string.replace(/<br\s*[\/]?>/gi, replacement || "");
+};
+
+// Usage.
+
+replace_br_tags("Hello<br/>world!"); // "Helloworld!"
+replace_br_tags("Hello<br/>world!", " "); // "Hello world!"
+
+// Non-function use.
 String.replace(/<br\s*[\/]?>/gi, "");
